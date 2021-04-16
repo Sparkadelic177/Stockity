d7b67d3ff51fa4a5072cf694566e9eb41deaf571
Sparky Evangelista <sparkyevangelista@gmail.com>
2021-04-16 18:18:19 -0400

    init push from branch

--- Frontend/TrendingStockApp/src/app/app.module.ts
+++ Frontend/TrendingStockApp/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { StockDetailsComponent } from './stock-details/stock-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StockDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Frontend/TrendingStockApp/src/app/stock-details/stock-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-details',
+  templateUrl: './stock-details.component.html',
+  styleUrls: ['./stock-details.component.css']
+})
+export class StockDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
